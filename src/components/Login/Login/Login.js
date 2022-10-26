5f55568305ccaba3b731b25418267a0013e4cf18
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-10-26 09:23:51 +0600

    Login dden

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -5,17 +5,41 @@ import { useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { AuthContext } from './../../../context/AuthProvider/AuthProvider';
+import {
+    FaGoogle,
+    FaGithub,
+    FaFacebook,
+    FaTwitter,
+    FaWhatsapp,
+    FaTwitch,
+  } from "react-icons/fa";
+import { ButtonGroup } from "react-bootstrap";
+import { GoogleAuthProvider } from "firebase/auth";    
 
 const Login = () => {
+    const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const { providerLogin } = useContext(AuthContext);
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoodleSignIn = () => {
+    providerLogin(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(`user`, user);
+        navigate(from, { replace: true });
+          toast.success("Login Successful");
+      })
+      .catch((error) => {
+        console.error(`error`, error);
+      });
+  };
 
   const [error, setError] = useState("");
   const [check, setCheck] = useState(false);
   const [red, setRed] = useState('btn-danger');
   
   const { signIn,logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const from = location.state?.from?.pathname || "/";
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -89,14 +113,14 @@ const Login = () => {
           type="checkbox"
           label={
             <>
-              Accept <Link to="/terms">terms and conditons.</Link>
+              Accept
             </>
           }
         />
       </Form.Group>
 
       <Button className={red} disabled={!check} variant="" type="submit">
-        Register
+        Login
       </Button>
       <Form.Text className="text-danger fw-bold">
         <p>{error}</p>
@@ -104,6 +128,22 @@ const Login = () => {
       <p className="link">
         Don't have an account? <Link to="/register">Register</Link> from here.
       </p>
+      <div className="text-center">
+      <h4 >OR</h4>
+      <h5>Login with</h5>
+      <ButtonGroup vertical className="shadow w-100 rounded p-2">
+        <Button
+          onClick={handleGoodleSignIn}
+          className="mb-4 rounded"
+          variant="outline-primary"
+        >
+          <FaGoogle></FaGoogle> <strong>Login with Google</strong>
+        </Button>
+        <Button variant="outline-secondary" className=" rounded">
+          <FaGithub></FaGithub> <strong>Login with GitHub</strong>
+        </Button>
+      </ButtonGroup>
+      </div>
     </Form>
   );
 };
